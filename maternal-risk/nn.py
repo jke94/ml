82b4ff19ad770457fc1f22fd8acbfcd767d8649a
jke94 <jcarracedo.dev@gmail.com>
2023-04-09 13:24:04 +0200

    Try to save in onnx format.

--- maternal-risk/nn.py
+++ maternal-risk/nn.py
@@ -1,37 +1,42 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, Normalizer
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
+from skl2onnx import convert_sklearn
+from skl2onnx.common.data_types import FloatTensorType
 
 # Read full data and select columns for PCA.
 df = pd.read_csv("MaternalHealthRiskDataSet.csv")
 
 selected_columns = ['Age', 'SystolicBP', 'DiastolicBP', 'BS', 'BodyTemp', 'HeartRate']
 
-dataframe = df[selected_columns]
+df_features = df[selected_columns]
 
-# Standardization
-min_max_scaler = MinMaxScaler()
-x_scaled = min_max_scaler.fit_transform(dataframe)
-df_scaled = pd.DataFrame(x_scaled, columns=selected_columns)
+# Features standardization
+min_max_scaler = StandardScaler()
+x_scaled = min_max_scaler.fit_transform(df_features)
+features_scaled = pd.DataFrame(x_scaled, columns=selected_columns)
+print(fea)
+
+# Labels
+labels = df['RiskLevel']
 
 # Dataset split: Train & Test.
-datasets = train_test_split(df_scaled, 
-                            df['RiskLevel'],
+datasets = train_test_split(features_scaled, 
+                            labels,
                             test_size=0.2)
 
 train_data, test_data, train_labels, test_labels = datasets
 
-clf = MLPClassifier(max_iter=6000,
-                    solver='lbfgs',
-                    # max_iter = 50,
-                    # activation='relu',
-                    alpha=1e-5,
-                    hidden_layer_sizes=(6,3), 
+clf = MLPClassifier(solver='lbfgs',
+                    max_iter=10000,
+                    activation='relu',
+                    alpha=1e-6,
+                    hidden_layer_sizes=(5,3), 
                     random_state=1)
 
 clf.fit(train_data, train_labels)
@@ -51,9 +56,9 @@ confusion_matrix(predictions_test, test_labels)
 print(classification_report(predictions_test, test_labels))
 
 # Save to onnx model.
-from skl2onnx import convert_sklearn
-from skl2onnx.common.data_types import FloatTensorType
-initial_type = [('float_input', FloatTensorType([None, 7]))]
-onx = convert_sklearn(classification_report, initial_types=initial_type)
+
+# TODO: Investigate correct FloatTensorType
+initial_type = [('float_input', FloatTensorType([None, 6]))]
+onx = convert_sklearn(clf, initial_types=initial_type)
 with open("maternal-risk.onnx", "wb") as f:
     f.write(onx.SerializeToString())
